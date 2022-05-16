9dcba3ccd1d1dd444abbe5e22cb85332e34927e2
Gabriel <gabriel04gh1.gh@gmail.com>
2022-05-15 22:59:18 -0300

    api

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3030;
 const router = require("./src/routes");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(router);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(port, () => console.log("Server listen on port", port));
 
